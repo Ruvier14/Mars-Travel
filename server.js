f74566316e6f394c6f4e6a4aa0689a12eecff429
Rowe <ruvier20144@gmail.com>
2024-12-20 00:15:02 +0800

    vercel chagneskfadf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'firstPage')));
+app.use('/', express.static(path.join(__dirname, 'firstPage')));
 app.use('/secPage', express.static(path.join(__dirname, 'secPage')));
 app.use('/thirPage', express.static(path.join(__dirname, 'thirPage')));
 
